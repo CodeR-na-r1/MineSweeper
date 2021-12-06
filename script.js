73bb057de69883ce40d5ddb73bb41b0fcdba516e
CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
2021-12-06 22:38:13 +0800

    Добавил комментарии, оптимизация и отладка кода

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
+/* Размеры поля */
 const WIDTH = 12;
 const HEIGHT = 12;
-let now_cell = 0;
-let field = [WIDTH * HEIGHT];
+let now_cell = 0; // Номер выделеннной ячейки
+let amount_open_cells = 0; // Количество открытых ячеек
+let field = [WIDTH * HEIGHT];  // Массив с данными о ячейках
 
+/* Создание поля */
 function init_field()
 {
-  let template_cell = document.getElementsByClassName("cell")[0];
-  let cell_contain = document.getElementsByClassName("field-container")[0];
+  let template_cell = document.getElementsByClassName("cell")[0];  // Шаблон ячейки
+  let cell_contain = document.getElementsByClassName("field-container")[0];  // Див-контейнер, в котором находятся ячейки
 
-  for (let i = 2; i < (WIDTH * HEIGHT) + 1; i++) {
-    let new_cell = template_cell.cloneNode(true);
-    new_cell.setAttribute('data-count', i-1);
+  for (let i = 2; i < (WIDTH * HEIGHT) + 1; i++) {  // Создание поля
+    let new_cell = template_cell.cloneNode(true);  // Клонируем шаблон
+    new_cell.setAttribute('data-count', i-1);  // Уникальный номер ячейки
     cell_contain.appendChild(new_cell);
-    if (i && i % 12 == 0)
+    if (i && i % WIDTH == 0)  // Перенос строки
       cell_contain.appendChild(document.createElement('br'));
   }
 
   now_cell = 0;
-  cell_contain.children[now_cell].classList.add('markered');
+  cell_contain.children[now_cell].classList.add('markered');  // По дефолту 1 ячейка является выделенной
 
-  cell_contain.addEventListener('click', init_cells);
+  cell_contain.addEventListener('click', init_cells);  // Событие на нажатие (на начало игры - 1 ход)
 
   return;
 }
 
+/* Размеры поля */
 function init_cells(event)
 {
-  for (let i = 0; i < WIDTH * HEIGHT; i++) {
-    if ((Math.trunc(Math.random() * 100) % 3) & 1)
-      field[i] = new Object( {'isBomb': true, 'user_marker': false} );
+  for (let i = 0; i < WIDTH * HEIGHT; i++) {  //
+    if (((Math.trunc(Math.random() * 100) % 3) & 1) && ((Math.trunc(Math.random() * 100) % 3) & 1))  //
+      field[i] = new Object( {'isBomb': true, 'user_marker': false} );  //
     else
-      field[i] = new Object( {'isBomb': false, 'user_marker': false} );
+      field[i] = new Object( {'isBomb': false, 'user_marker': false} );  //
   }
 
-  if (event["path"][0].getAttribute('data-count') != null)
+  if (event["path"][0].getAttribute('data-count') != null)  //
   {
-    document.getElementsByClassName("field-container")[0].removeEventListener('click', init_cells);
-    let click_elem = event["path"][0];
-    field[Number(click_elem.getAttribute('data-count'))].isBomb = false;
-    let count = calc_bomb_around(click_elem);
-    show_bomb_around(click_elem, count);
+    document.getElementsByClassName("field-container")[0].removeEventListener('click', init_cells);  // Убираем событие для 1 хода
+    let click_elem = event["path"][0];   // Элемент, куда нажал пользователь
+    field[Number(click_elem.getAttribute('data-count'))].isBomb = false;  // 1 ход - обязательно на мирную ячейку
+    let count = calc_bomb_around(click_elem);  // считаем кол-во бомб вокруг нее
+    show_bomb_around(click_elem, count);  // отображаем кол-во бомб вокруг нее
 
-    update_marker_cell(click_elem);
+    update_marker_cell(click_elem);   // Метим ячейку, куда нажал пользователь
+    ++amount_open_cells;  // Первая ячейка, что открыл пользователь
 
+    // Добавляем события на нажатия левой, правой кнопки мыши и клавиатуры соответсвенно
     document.getElementsByClassName("field-container")[0].addEventListener('click', processing_move);
     document.getElementsByClassName("field-container")[0].addEventListener('contextmenu', mark_a_cell);
     document.addEventListener('keydown', events_keyboard);
@@ -51,37 +57,50 @@ function init_cells(event)
   return;
 }
 
+/* Ход пользователя (левой кнопкой мыши === открытие ячейки) */
 function processing_move(event)
 {
-  let event_elem;
-  if (event && event['path'][0].getAttribute('data-count') != null)
+  let event_elem;   // Целевая ячейка
+  if (event && event['path'][0].getAttribute('data-count') != null)   // Если событие с мыши - берем по событию
   {
     event_elem = event['path'][0];
   }
   else {
-    event_elem = document.getElementsByClassName("field-container")[0].children[now_cell + Math.trunc(now_cell/WIDTH)];
+    event_elem = document.getElementsByClassName("field-container")[0].children[now_cell + Math.trunc(now_cell/WIDTH)];   // Иначе берем по списку дочерних у общего контейнера (в индексе учитывается наличие в списке тегов переноса строки (<br>))
   }
 
-  let num_elem = Number(event_elem.getAttribute('data-count'));
+  let num_elem = Number(event_elem.getAttribute('data-count'));   // уникальный номер ячейки
 
-  if (event_elem.getAttribute('data-count') != null)
+  if (event_elem.getAttribute('data-count') != null)   // Если событие точно на ячейку (а не на фон)
   {
-    update_marker_cell(event_elem);
+    update_marker_cell(event_elem);   // Метим ячейку, куда нажал пользователь
 
-    if (field[num_elem].user_marker == true)
+    if (field[num_elem].user_marker == true)   // Если на ней метка (флажок), то не трогаем
      return;
 
-    if (field[num_elem].isBomb == false)
+     if (amount_open_cells == WIDTH * HEIGHT)   // Если все ячейки выделены, проверяем условие выигрыша
     {
-      let count = calc_bomb_around(event_elem);
-      show_bomb_around(event_elem, count);
+      // Проверки, что все помеченные ячейки не являються бомбами
+      // Поздравление о выигрыше
+      // Comming soon
+
+      //return;
     }
-    else if (field[num_elem].isBomb == true)
+
+    if (field[num_elem].isBomb == false)   // Если не бомба
     {
-      console.log('bomb');
-      document.getElementsByClassName("field-container")[0].removeEventListener('click', processing_move);
-      document.getElementsByClassName("field-container")[0].removeEventListener('contextmenu', mark_a_cell);
-      document.removeEventListener('keydown', events_keyboard);
+      let count = calc_bomb_around(event_elem);   // Считаем, сколько бомб вокруг
+      show_bomb_around(event_elem, count);   // выводим сколько бомб вокруг
+      ++amount_open_cells;
+    }
+    else if (field[num_elem].isBomb == true)   // Если бомба
+    {
+      console.log('bomb');   // Отладочный рудимент
+      // Отключаем события на нажатия левой, правой кнопки мыши и клавиатуры соответсвенно
+      //document.getElementsByClassName("field-container")[0].removeEventListener('click', processing_move);
+      //document.getElementsByClassName("field-container")[0].removeEventListener('contextmenu', mark_a_cell);
+      //document.removeEventListener('keydown', events_keyboard);
+      // Добавляем изображение бомбы
       event_elem.style.backgroundImage = 'url(data_pictures/bomb.svg)';
       event_elem.style.backgroundColor = 'red';
     }
@@ -90,25 +109,28 @@ function processing_move(event)
   return;
 }
 
+/* Подсчет количество бомб вокруг целевой ячейки */
 function calc_bomb_around(event_elem)
 {
-  let i = Number(event_elem.getAttribute('data-count'));
+  let i = Number(event_elem.getAttribute('data-count'));   // Уникальный номер целевой ячейки
 
-  let count = 0;
-  if (i % 12 != 0 && i - WIDTH - 1 > 0 && field[i - WIDTH - 1].isBomb) { ++count; }
-  if (i - WIDTH > 0 && field[i - WIDTH].isBomb) { ++count; }
-  if ((i+1) % 12 != 0 && i - WIDTH + 1 > 0 && field[i - WIDTH + 1].isBomb) { ++count; }
+  let count = 0;   // Инициализая счетчика бомб
+  /* В условиях учитывается крайнее положение ячейки */
+  if (i % 12 != 0 && i - WIDTH - 1 > 0 && field[i - WIDTH - 1].isBomb) { ++count; }   // вверху слева
+  if (i - WIDTH > 0 && field[i - WIDTH].isBomb) { ++count; }   // вверху
+  if ((i+1) % 12 != 0 && i - WIDTH + 1 > 0 && field[i - WIDTH + 1].isBomb) { ++count; }   // вверху справа
 
-  if (i % 12 != 0 && i - 1 > 0 && field[i - 1].isBomb) { ++count; }
-  if ((i+1) % 12 != 0 && i + 1 > 0 && field[i + 1].isBomb) { ++count; }
+  if ((i != 0 && i % 12 != 0) && i - 1 > 0 && field[i - 1].isBomb) { ++count; }   // слева
+  if ((i+1) % 12 != 0 && i + 1 > 0 && field[i + 1].isBomb) { ++count; }   // справа
 
-  if (i % 12 != 0 && i + WIDTH - 1 < WIDTH*HEIGHT && field[i + WIDTH - 1].isBomb) { ++count; }
-  if (i + WIDTH < WIDTH*HEIGHT && field[i + WIDTH].isBomb) { ++count; }
-  if ((i+1) % 12 != 0 && i + WIDTH + 1 < WIDTH*HEIGHT && field[i + WIDTH + 1].isBomb) { ++count; }
+  if (i % 12 != 0 && i + WIDTH - 1 < WIDTH*HEIGHT && field[i + WIDTH - 1].isBomb) { ++count; }   // снизу слева
+  if (i + WIDTH < WIDTH*HEIGHT && field[i + WIDTH].isBomb) { ++count; }   // снизу
+  if ((i+1) % 12 != 0 && i + WIDTH + 1 < WIDTH*HEIGHT && field[i + WIDTH + 1].isBomb) { ++count; }   // снизу справа
 
   return count;
 }
 
+/* Отображние количества бомб вокруг целевой ячейки*/
 function show_bomb_around(event_elem, count)
 {
   let span = document.createElement('span');
@@ -119,71 +141,78 @@ function show_bomb_around(event_elem, count)
   return;
 }
 
+/* Создание метки для ячейки (флажок) */
 function mark_a_cell(event)
 {
   let cell;
-  if (event && event['path'][0].getAttribute('data-count') != null)
+  if (event && event['path'][0].getAttribute('data-count') != null)   // Если событие с мыши
   {
     cell = event['path'][0];
   }
-  else {
+  else {   // иначе если с клавиатуры
       cell = document.getElementsByClassName("field-container")[0].children[now_cell + Math.trunc(now_cell/WIDTH)];
   }
 
-  if (cell.getAttribute('data-count') != null)
+  if (cell.getAttribute('data-count') != null)   // Если событие точно на ячейку
   {
-    if (event) event.preventDefault();
-    if (field[Number(cell.getAttribute('data-count'))].user_marker)
+    if (event) event.preventDefault();   // Если это событие с мыши, то отключаем появление контекстного меню
+    if (field[Number(cell.getAttribute('data-count'))].user_marker)   // Если метка уже была
     {
-      field[Number(cell.getAttribute('data-count'))].user_marker = false;
-      cell.style.backgroundImage = "";
+      --amount_open_cells;  // Вычет ячейки из общего количества открытых (обработанных) ячеек
+      field[Number(cell.getAttribute('data-count'))].user_marker = false;   // меняем флаг в свойтсвах ячейки
+      cell.style.backgroundImage = "";   // Убираем флаг с фона ячейки
     }
-    else
+    else   // Если метки не было
       {
-        field[Number(cell.getAttribute('data-count'))].user_marker = true;
-        cell.style.backgroundImage = "url(data_pictures/flag.svg)";
+        ++amount_open_cells;  // Добавление ячейки в общее количество открытых (обработанных) ячеек
+        field[Number(cell.getAttribute('data-count'))].user_marker = true;   // меняем флаг в свойтсвах ячейки
+        cell.style.backgroundImage = "url(data_pictures/flag.svg)";   // Ставим флаг на фон ячейки
       }
   }
 
   return;
 }
 
+/* Обновление показа выбранной пользователем ячейки  */
 function update_marker_cell(event_elem)
 {
-  let index = Math.trunc(now_cell + now_cell/WIDTH);
-  event_elem.parentNode.children[index].classList.remove('markered');
+  let index = Math.trunc(now_cell + now_cell/WIDTH);   // Находим индекс (порядковый номер среди дочерних элементов контейнера) прошлой выбранной ячейки
+  event_elem.parentNode.children[index].classList.remove('markered');   // Удаляем стилизующий класс
 
-  now_cell = Number(event_elem.getAttribute('data-count'));
-  index = Math.trunc(now_cell + now_cell/WIDTH);
-  event_elem.parentNode.children[index].classList.add('markered');
+  now_cell = Number(event_elem.getAttribute('data-count'));   // Достаем индекс (уникальный номер текущей выбранной ячейки)
+  index = Math.trunc(now_cell + now_cell/WIDTH);   // Рассчитываем по индексу порядковый номер среди дочерних элементов контейнера
+  event_elem.parentNode.children[index].classList.add('markered');   // Добавляем стилизующий класс
 
   return;
 }
 
+/* Обработка событий с клавиатуры */
 function events_keyboard(event)
 {
-  event.preventDefault();
-  let elem_contain_cells = document.getElementsByClassName("field-container")[0];
-  let index = Math.trunc(now_cell + now_cell/WIDTH);
+  event.preventDefault();   // Перехват события
+  let elem_contain_cells = document.getElementsByClassName("field-container")[0];   // Контейнер с ячейками
+  let index = Math.trunc(now_cell + now_cell/WIDTH);   // порядковый номер среди дочерних элементов контейнера
 
-  if (event.key=='ArrowLeft')
+  /* У стрелок влево и вправо учитывается перенос выделения ячейки на нужную строку */
+    /* + у всех стрелок учитывается наличие в массиве мешающих тегов переноса строки (<br>) */
+  if (event.key=='ArrowLeft')   // Кнопка Стрелки влево
   {
     index = (index - Math.trunc(index/WIDTH)) % 12 == 0 ? index-2 : index-1;
     update_marker_cell(elem_contain_cells.children[index]);
-  } else if (event.key=='ArrowRight')
+  } else if (event.key=='ArrowRight')   // Кнопка Стрелки вправо
   {
     index = index == (WIDTH-1 + Math.trunc((index-Math.trunc(index/WIDTH))/WIDTH)*(WIDTH+1)) ? index+2 : index+1;
     update_marker_cell(elem_contain_cells.children[index]);
-  } else if (event.key=='ArrowUp')
+  } else if (event.key=='ArrowUp')   // Кнопка Стрелки вврех
   {
     update_marker_cell(elem_contain_cells.children[index - WIDTH - 1]);
-  } else if (event.key=='ArrowDown')
+  } else if (event.key=='ArrowDown')   // Кнопка Стрелки вниз
   {
     update_marker_cell(elem_contain_cells.children[index + WIDTH + 1]);
-  } else if (event.ctrlKey && (event.key==' '|| event.key=='Enter'))
+  } else if (event.ctrlKey && (event.key==' '|| event.key=='Enter'))   // Кнопка пробел или enter с зажатым ctrl
   {
     mark_a_cell(null);
-  } else if (event.key==' ' || event.key=='Enter')
+  } else if (event.key==' ' || event.key=='Enter')   // Кнопка пробел или enter
   {
     processing_move(null);
   }
@@ -192,4 +221,4 @@ function events_keyboard(event)
   return;
 }
 
-init_field();
+init_field(); // Запуск функции создания поля
